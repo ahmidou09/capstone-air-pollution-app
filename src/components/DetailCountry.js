fafd896191c70ec34a150f66f294efb652ca2774
Ahmidou EL mehdi <ahmidouem@gmail.com>
2023-09-04 21:02:32 +0100

    Implemented user-friendly filtering options

--- src/components/DetailCountry.js
+++ src/components/DetailCountry.js
@@ -1,14 +1,33 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router';
+import { fetchAirPollution } from '../redux/airPollution/airPollutionSlice';
 
 const DetailCountry = () => {
-  const { country } = useParams();
+  const { country, lat, lon } = useParams();
+  const dispatch = useDispatch();
+  const { data, loading, error } = useSelector((state) => state.airPollution);
+
+  useEffect(() => {
+    dispatch(fetchAirPollution({ lat, lon }));
+  }, [dispatch, lat, lon]);
 
   return (
-    <h3>
-      country details :
-      {country}
-    </h3>
+    <div>
+      {loading && <div>Loading...</div>}
+      {error && (
+      <div>
+        Error:
+        {error}
+      </div>
+      )}
+      {data && (
+        <div>
+          <h2>{country}</h2>
+          <div>{data.list[0].main.aqi}</div>
+        </div>
+      )}
+    </div>
   );
 };
 
--- src/components/Region.js
+++ src/components/Region.js
@@ -1,20 +1,33 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router';
 import { Link } from 'react-router-dom';
+import { fetchCountries } from '../redux/countries/countriesSlice';
 
 const Region = () => {
-  const contryList = ['morocco', 'togo', 'egypt'];
   const { region } = useParams();
+  const dispatch = useDispatch();
+  const { loading, data, error } = useSelector((state) => state.countries);
+
+  useEffect(() => {
+    dispatch(fetchCountries(region));
+  }, [dispatch, region]);
 
   return (
-    <div>
-      <h1>{ region }</h1>
-      {contryList.map((country) => (
-        <div key={country}>
-          <Link to={`/${region}/${country}`}>{country}</Link>
-        </div>
-      ))}
-    </div>
+    <>
+      <div>
+        <h1>{region}</h1>
+        {loading && <div>loading...</div>}
+        {error && <div>error</div>}
+        {!loading && data.map((country) => (
+          <div key={country.name.common}>
+            <Link to={`/${region}/${country.name.common}/${country.latlng[0]}/${country.latlng[1]}`}>
+              {country.name.common}
+            </Link>
+          </div>
+        ))}
+      </div>
+    </>
   );
 };
 
